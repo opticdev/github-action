0bcfe0b453cdc362a925b5a7129766577c39edf6
Alan Johnson <alan.johnson@useoptic.com>
2022-06-27 11:58:55 -0400

    Built rough ability to install optic-ci

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,47 @@
 import * as core from "@actions/core";
+import * as exec from "@actions/exec";
 
 const token = core.getInput("token");
 
-console.log(token);
+async function runAction(): Promise<void> {
+  const valid = verifyInput();
+  if (!valid) {
+    return;
+  }
+
+  const installed = install();
+  if (!installed) {
+    return;
+  }
+}
+
+async function verifyInput(): Promise<boolean> {
+  if (!token) {
+    core.error("No token provided");
+    return false;
+  }
+
+  return true;
+}
+
+async function install() {
+  try {
+    core.info("Installing optic-ci");
+
+    // todo: wrap github's exec with a standard way to capture input and only
+    // show it on error
+    await exec.exec("npm", [
+      "install",
+      "--location=global",
+      "@useoptic/optic-ci",
+    ]);
+  } catch (e) {
+    if (e instanceof Error) {
+      core.error(e);
+    }
+
+    return false;
+  }
+}
+
+runAction().then(() => null);
